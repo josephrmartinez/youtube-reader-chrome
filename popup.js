b20698d5b6233e57ba2de7644db36da8d76bc3d3
Joseph Martinez <joseph.r.martinez@gmail.com>
2023-11-12 17:31:00 -0700

    Update content script

--- popup.js
+++ popup.js
@@ -1,149 +1,143 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const taskInput = document.getElementById('taskInput');
-    const generateButton = document.getElementById('generateButton');
-    const trashButton = document.getElementById('trash')
-    const clipboardButton = document.getElementById('clipboard');
+document.addEventListener('DOMContentLoaded', () => {
+  const taskInput = document.getElementById('taskInput');
+  const generateButton = document.getElementById('generateButton');
+  const trashButton = document.getElementById('trash')
+  const clipboardButton = document.getElementById('clipboard');
 
-    trashButton.addEventListener('click', function(){
-      chrome.storage.local.remove('persistedText', function() {
-        // After removing the item, update the popup text
-        updatePopupText("");
-      })
-    })
+  trashButton.addEventListener('click', () => {
+    chrome.storage.local.remove('persistedText', () => {
+      // After removing the item, update the popup text
+      updatePopupText('');
+    });
+  });
 
-
-    
-
-    clipboardButton.addEventListener('click', function(){
-      chrome.storage.local.get(['persistedText'], function(result) {
-        const persistedText = result.persistedText;
-        if (persistedText) {
-          const extractedText = extractTextFromHTML(persistedText);
-
-          // Use the Clipboard API to copy the text to the clipboard
-          navigator.clipboard.writeText(extractedText)
-            .then(() => {
-              const imgElement = clipboardButton.querySelector('img');
-
-              // Change the src attribute to images/check.png
-              imgElement.src = 'images/check.png';
-
-              // Set a timeout to revert the src attribute after two seconds
-              setTimeout(function () {
-                imgElement.src = 'images/clipboard.png';
-              }, 2000);
-            })
-            .catch((err) => {
-              console.error('Unable to copy text to clipboard', err);
-            });
-        }
-      })   
-    })
-
-    function extractTextFromHTML(htmlString) {
-      const parser = new DOMParser();
-      const doc = parser.parseFromString(htmlString, 'text/html');
-  
-      // Get all text nodes within the document body
-      const textNodes = document.createTreeWalker(
-          doc.body,
-          NodeFilter.SHOW_TEXT,
-          null,
-          false
-      );
-  
-      let extractedText = '';
-  
-      // Iterate through text nodes and concatenate their text content
-      while (textNodes.nextNode()) {
-        const nodeValue = textNodes.currentNode.nodeValue.trim();
-        if (nodeValue !== '') {
-          extractedText += nodeValue + '\n';
-        }
-      }
-      return extractedText.trim();
-  }
-  
-  
-  
-
-    // Load the persisted text when the popup is opened
-    chrome.storage.local.get(['persistedText'], function(result) {
+  clipboardButton.addEventListener('click', () => {
+    chrome.storage.local.get(['persistedText'], (result) => {
       const persistedText = result.persistedText;
       if (persistedText) {
-        updatePopupText(persistedText);
-      }
-      
-  });
-  
-
-
-    generateButton.addEventListener('click', function() {
-      const task = taskInput.value;
-      chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
-        const videoId = extractVideoId(tabs[0].url);
-        if (videoId && task) {
-          // Send data to the API endpoint
-          sendDataToAPI(videoId, task);
-        } else {
-          console.error('Failed to extract video ID from the YouTube page URL or no task defined');
-        }
-      });
-    });
-  
-    function extractVideoId(url) {
-      const videoIdMatch = url.match(/v=([A-Za-z0-9_\-]+)/);
-      if (videoIdMatch) {
-        return videoIdMatch[1];
-      }
-      return null;
-    }
-  
-    function sendDataToAPI(videoId, task) {
-      updatePopupText("")
-      
-      const generateButtonImg = generateButton.querySelector('img');
-
-      // Change the src attribute to the loading indicator
-      generateButtonImg.src = 'images/spinner-gap.png';
-      generateButtonImg.classList.add('loading-spinner');
-
-      fetch('http://127.0.0.1:8000/api/perform-task', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ url: videoId, task: task }),
-      })
-        .then(response => response.json())
-        .then(data => {
-          // Handle the response from your server...
-          const completionText = data.completion.text;
-
-          // Save the text to local storage
-          chrome.storage.local.set({ 'persistedText': completionText }, function() {
-            updatePopupText(completionText);
+        const extractedText = extractTextFromHTML(persistedText);
+        
+        // Use the Clipboard API to copy the text to the clipboard
+        navigator.clipboard.writeText(extractedText)
+          .then(() => {
+            // Change the src attribute to images/check.png to indicate action
+            const imgElement = clipboardButton.querySelector('img');
+            imgElement.src = 'images/check.png';
+            // Set a timeout to revert the src attribute after two seconds
+            setTimeout(() => {
+              imgElement.src = 'images/clipboard.png';
+            }, 2000);
+          })
+          .catch((err) => {
+            console.error('Unable to copy text to clipboard', err);
           });
-        })
-        .catch(error => {
-          console.error('Error:', error);
-        })
-        .finally(() => {
-          taskInput.value = "";
-          // Revert the src attribute after fetch request complete
-          generateButtonImg.classList.remove('loading-spinner');
-          generateButtonImg.src = 'images/send.png'
-        });
+      }
+    });
+  });
+
+  function extractTextFromHTML(htmlString) {
+    const parser = new DOMParser();
+    const doc = parser.parseFromString(htmlString, 'text/html');
+
+    // Get all text nodes within the document body
+    const textNodes = document.createTreeWalker(
+        doc.body,
+        NodeFilter.SHOW_TEXT,
+        null,
+        false
+    );
+
+    let extractedText = '';
+
+    // Iterate through text nodes and concatenate their text content
+    while (textNodes.nextNode()) {
+      const nodeValue = textNodes.currentNode.nodeValue.trim();
+      if (nodeValue !== '') {
+        extractedText += nodeValue + '\n';
+      }
+    }
+    return extractedText.trim();
+}
+
+
+
+
+  // Load the persisted text when the popup is opened
+  chrome.storage.local.get(['persistedText'], (result) => {
+    const persistedText = result.persistedText;
+    if (persistedText) {
+      updatePopupText(persistedText);
+    }
+  });
+
+
+
+  generateButton.addEventListener('click', function() {
+    const task = taskInput.value;
+    chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+      const videoId = extractVideoId(tabs[0].url);
+      if (videoId && task) {
+        // Send data to the API endpoint
+        sendDataToAPI(videoId, task);
+      } else {
+        console.error('Failed to extract video ID from the YouTube page URL or no task defined');
+      }
+    });
+  });
+
+  function extractVideoId(url) {
+    const videoIdMatch = url.match(/v=([A-Za-z0-9_\-]+)/);
+    if (videoIdMatch) {
+      return videoIdMatch[1];
+    }
+    return null;
   }
 
+  function sendDataToAPI(videoId, task) {
+    updatePopupText("")
+    
+    const generateButtonImg = generateButton.querySelector('img');
 
-    // Function to update the text in your popup
-    function updatePopupText(text) {
-        // Assuming you have an element with the ID "popupText" to display the text
-        const popupTextElement = document.getElementById('popupText');
-        if (popupTextElement) {
-        popupTextElement.innerHTML = text;
-        }
-    }
+    // Change the src attribute to the loading indicator
+    generateButtonImg.src = 'images/spinner-gap.png';
+    generateButtonImg.classList.add('loading-spinner');
+
+    fetch('http://127.0.0.1:8000/api/perform-task', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ url: videoId, task: task }),
+    })
+      .then(response => response.json())
+      .then(data => {
+        // Handle the response from your server...
+        const completionText = data.completion.text;
+
+        // Save the text to local storage
+        chrome.storage.local.set({ 'persistedText': completionText }, function() {
+          updatePopupText(completionText);
+        });
+      })
+      .catch(error => {
+        console.error('Error:', error);
+      })
+      .finally(() => {
+        taskInput.value = "";
+        // Revert the src attribute after fetch request complete
+        generateButtonImg.classList.remove('loading-spinner');
+        generateButtonImg.src = 'images/send.png'
+      });
+}
+
+
+  // Function to update the text in your popup
+  function updatePopupText(text) {
+      // Assuming you have an element with the ID "popupText" to display the text
+      const popupTextElement = document.getElementById('popupText');
+      if (popupTextElement) {
+      popupTextElement.innerHTML = text;
+      }
+  }
 
   });
--- scripts/content.js
+++ scripts/content.js
@@ -1,52 +1,13 @@
+// document.addEventListener('yt-navigate-finish', process);
+// // Choose a different event depending on when you want to apply the change
+// // document.addEventListener('yt-navigate-finish', process);
 
-// var parentDiv = document.getElementById("secondary"); // the parent div
-// var newDiv = document.createElement("div"); // the new div to be inserted
-// newDiv.textContent = "This is a new div";
+// if (document.body) process();
+// else 
 
-// if (parentDiv.firstChild) {
-//  parentDiv.insertBefore(newDiv, parentDiv.firstChild);
-// } else {
-//  parentDiv.appendChild(newDiv);
-// }
+document.addEventListener('DOMContentLoaded', process);
 
-  
-// const textField = document.createElement("input");
-// textField.type = "text";
-// textField.id = "myTextField"; // Add an ID to access this element later
-
-// const button = document.createElement("button");
-// button.textContent = "Generate";
-// // button.classList.add("yt-spec-touch-feedback-shape__fill")
-// button.id = "myButton"; // Add an ID to access this element later
-
-// button.addEventListener("click", function () {
-//   // Add your code here to generate a blog post or perform any other action
-//   alert("Button clicked!");
-// });
-
-
-
-
-
-// const article = document.querySelector("article");
-
-// // `document.querySelector` may return null if the selector doesn't match anything.
-// if (article) {
-//   const text = article.textContent;
-//   const wordMatchRegExp = /[^\s]+/g; // Regular expression
-//   const words = text.matchAll(wordMatchRegExp);
-//   // matchAll returns an iterator, convert to array to get word count
-//   const wordCount = [...words].length;
-//   const readingTime = Math.round(wordCount / 200);
-//   const badge = document.createElement("p");
-//   // Use the same styling as the publish information in an article's header
-//   badge.classList.add("color-secondary-text", "type--caption");
-//   badge.textContent = `⏱️ ${readingTime} min read`;
-
-//   // Support for API reference docs
-//   const heading = article.querySelector("h1");
-//   // Support for article docs with date
-//   const date = article.querySelector("time")?.parentNode;
-
-//   (date ?? heading).insertAdjacentElement("afterend", badge);
-// }
+function process() {
+  const primary = document.querySelector("#primary")
+  console.log("primary", primary)
+};
